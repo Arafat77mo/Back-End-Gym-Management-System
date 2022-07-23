5f5aa4579d459bea4aa7a2fb024aeef5f1e48449
Arafat77mo <mohammed.poop444@gmail.com>
2022-07-23 18:12:36 +0200

    admin && user --> login & logout & register & user-profile

--- GYM/config/auth.php
+++ GYM/config/auth.php
@@ -42,6 +42,29 @@ return [
         ],
     ],
 
+
+    
+        'defaults' => [
+            'guard' => 'api',
+            'passwords' => 'users',
+        ],
+    
+        'guards' => [
+            'web' => [
+                'driver' => 'session',
+                'provider' => 'users',
+            ],
+            'api' => [
+                'driver' => 'jwt',
+                'provider' => 'users',
+                'hash' => false,
+            ],
+            'admin-api' => [
+                'driver' => 'jwt',
+                'provider' => 'admins',
+                'hash' => false,
+            ],
+        ],
     /*
     |--------------------------------------------------------------------------
     | User Providers
@@ -63,6 +86,10 @@ return [
         'users' => [
             'driver' => 'eloquent',
             'model' => App\Models\User::class,
+        ],
+        'admins' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\Admin::class,
         ],
 
         // 'users' => [
